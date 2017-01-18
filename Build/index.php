623a5d8da9487ac056463419bf70aad5abab82cf
Антон Мелентьев <viktorina.online@yandex.ru>
2017-01-18 15:19:05 +0000

    index

--- Build/index.php
+++ Build/index.php
@@ -7,7 +7,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name=viewport content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta name="openstat-verification" content="cfef942ea53193c4c1de81d2422fb9750188ea23" />
 <meta name="google-site-verification" content="5YJskvLvruK9wQ_tTm9RNDMXmN_O95T_lPu6JnEAMGI" />
 <meta name="msvalidate.01" content="59160E97CE1E7171F42872CD78D31347" />
@@ -21,45 +21,42 @@
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://xn--80adsajtfqq.xn--80asehdb/" />
 <meta property="og:image" content="https://xn--80adsajtfqq.xn--80asehdb/images/account/admin.jpg" />
-<link rel="stylesheet" type="text/css" href="css/style_allvopr.css" />
-<link rel="stylesheet" type="text/css" href="css/start.css" />
 <link rel="stylesheet" href="css/odometer-theme-default.css" />
 <link rel="stylesheet" href="css/supersized.css" type="text/css" />
+<link rel="stylesheet" href="css/start.css" />
 <link rel="shortcut icon" href="images/favicon.png" />
 <? require_once "lib/countersm.php";?>
 </head>
-<div class="s w100 pa">
-	<div class="p dib taz">
-		<h1 class="p30">Викторина Онлайн</h1>
-		<noindex><a class="b dib cur tdn trans fs18 gray brad" title="Играть" href="Вопросы">Играть</a></noindex>
-		<? if(!$_SESSION['login']) { ?>
-		<noindex><a class="b dib cur tdn trans fs18 gray brad" title="Авторизация" onclick="show('log','body')">Авторизация</a></noindex>
-		<? } ?>
-		<h2 id="seo" class="gray w80 fs12 mob p30">
-			Представляем вам интеллектуальную игру онлайн без  регистрации. Здесь  вы найдете много интересного: научные факты, головоломки,  логические загадки и сложные вопросы с подвохом. На нашем сайте вы можете обнаружить и шуточные, провокационные задания. На каждый каверзный вопрос даны четыре варианта ответа, из которых требуется выбрать лишь один, правильный.
-   Задать свои вопросы и проверить чужие ответы может любой участник!
-		</h2>
-		<div class="dib w80 p10 fs20 fwb mob">
-			<span class="dib green">Участники викторины дали&nbsp;</span>
-				<div class="odometer red">10000</div>
-			<span class="dib green">&nbsp;ответов!</span>
-		</div>
-
-	</div>
-</div>
-<nav itemscope itemtype="http://www.schema.org/SiteNavigationElement" id="page-menu" class="ht mob w100 pa" role="navigation" >
+<nav itemscope itemtype="http://www.schema.org/SiteNavigationElement" id="page-menu" class="taz mob w100" role="navigation" >
 	<a itemprop="url" class="tdn trans fwb gray fs18" href="Вопросы" title="Вопросы">Вопросы<noindex><span title="Количество новых вопросов, добавленных за последние 24 часа" class="pa green fs10 newdata help">+<?echo NewVopr();?></span></noindex></a>
 	<a itemprop="url" class="tdn trans fwb gray fs18" href="Участники" title="Участники">Участники<?if(NewUser()){?><noindex><span title="Количество новых участников, зарегистрировавшихся за последние 24 часа" class="pa green fs10 newdata help">+<?echo NewUser();?></span></noindex><?}?></a>
 	<a itemprop="url" class="tdn trans fwb gray fs18" href="Контакты" title="Контакты">Контакты</a>
 </nav>
-<div class="hb mob pa">
-	<div class="copyright">
-		<p class="name fs16 fwb gray">&copy;Викторина.Онлайн 2016</p>
-		<a class="map cur tdn fwb gray" title="Карта сайта" href="/Карта_сайта">Карта сайта</a>
+<div class="p w100 taz">
+	<h1 class="p30">Викторина Онлайн</h1>
+	<noindex><a class="b dib cur tdn trans fs18 gray brad" title="Играть" href="Вопросы">Играть</a></noindex>
+	<? if(!$_SESSION['login']) { ?>
+	<noindex><a class="b dib cur tdn trans fs18 gray brad" title="Авторизация" onclick="show('log','body')">Авторизация</a></noindex>
+	<? } ?>
+	<h2 class="gray fs12 mob p30">
+		Представляем вам интеллектуальную игру онлайн без регистрации. Здесь вы&nbsp;найдете много интересного: научные факты, головоломки,</br> логические загадки и&nbsp;сложные вопросы с&nbsp;подвохом. На&nbsp;нашем сайте вы&nbsp;можете обнаружить и&nbsp;шуточные, провокационные задания.</br>На&nbsp;каждый каверзный вопрос даны четыре варианта ответа, из&nbsp;которых требуется выбрать лишь один, правильный.</br>Задать свои вопросы и&nbsp;проверить чужие ответы может любой участник!
+	</h2>
+	<div class="fs20 fwb mob">
+		<span class="dib green">Участники викторины дали&nbsp;</span>
+			<div class="odometer red">10000</div>
+		<span class="dib green">&nbsp;ответов!</span>
 	</div>
-	<a id="l_fb" class="trans brad" title="Группа в Facebook" target="_blank" href="https://www.facebook.com/viktorina.online/"></a>
-	<a id="l_ok" class="trans brad" title="Группа в Одноклассники" target="_blank" href="https://ok.ru/group/52718353252445"></a>
-	<a id="l_vk" class="trans brad" title="Группа в ВКонтакте" target="_blank" href="https://vk.com/viktorina.online"></a>
+	</div>
+<div class="w100 mob pa taz">
+	<div class="l_bl p30">
+		<p class="name fs16 fwb gray">&copy;Викторина.Онлайн 2016</p>
+		<a class="map trans cur tdn fwb gray" title="Карта сайта" href="Карта_сайта">Карта сайта</a>
+	</div>
+	<div class="r_bl p30">
+		<a id="l_fb" class="trans brad" title="Группа в Facebook" target="_blank" href="https://www.facebook.com/viktorina.online/"></a>
+		<a id="l_ok" class="trans brad" title="Группа в Одноклассники" target="_blank" href="https://ok.ru/group/52718353252445"></a>
+		<a id="l_vk" class="trans brad" title="Группа в ВКонтакте" target="_blank" href="https://vk.com/viktorina.online"></a>
+	</div>
 </div>
 <script src="js/jquery-1.12.2.min.js"></script>
 <script src="lib/start.js"></script>
